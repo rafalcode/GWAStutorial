bdb888e1d965b1a92316479fba0d28bf74d5421c
rf73 <Ramon Fallon>
2019-06-29 17:19:20 +0100

    pedsxcond.R expanded to allow for bed files too, not tested though, possibly some small errors

--- pedsxcond.R
+++ pedsxcond.R
@@ -10,19 +10,61 @@ if(numargs != enumargs) {
     stop("Stopping right here")
 }
 
+# first look for ped files, it's like though, that
 inpedf <- paste0(args[1], ".ped")
+foundped =0
 inmapf <- paste0(args[1], ".map")
 if(!file.exists(inpedf)) {
-    stop(paste0("Error: cannot find the input file \"", inpedf, "\".\n"))
+    warning(paste0("Error: cannot find the input file \"", inpedf, "\".\n"))
+} else {
+    foundped =1
 }
 if(!file.exists(inmapf)) {
-    stop(paste0("Error: \"", inmapf, "\" not found. Although not required, it is a mistake to have a ped file without an accompanying map file.\n"))
+    warning(paste0("Error: \"", inmapf, "\" not found. Although not required, it is a mistake to have a ped file without an accompanying map file.\n"))
 }
 
+if(!foundped) {
+    print("So, no ped file was found with the that prefix, try the bed/bim/fam trio")
+    inbimf <- paste0(args[1], ".bim")
+    inbedf <- paste0(args[1], ".bed")
+    infamf <- paste0(args[1], ".fam")
+
+    if(!file.exists(inbedf)) {
+        stop(paste0("Error: cannot find the input file \"", inbedf, "\".\n"))
+    }
+    if((!file.exists(inbimf)) | (!file.exists(infamf))) {
+        stop(paste0("Error: \"", inbimf, "\" or \"", infamf, "\" not found. Although not required, it is a mistake to have a bed file without accompanying bim and fam files.\n"))
+    }
+
+    # so now we're going to use plink to convert the bed/bim/fam to ped
+    plinkbin <- Sys.which("plink1.9")
+    humopts <- " --silent --allow-no-sex"
+    plinkCmd <- paste0(plinkbin, humopts, " --bfile ", args[1], " --recode --out ", args[1])
+    rt <- system(plinkCmd, intern=FALSE)
+    if (rt!=0) {
+        stop("plink bed to ped error.")
+    }
+}
+
+# OK, even if we had only bed, it's now been convert to ped
 outpf <- paste0(args[1], "_femcase.ped")
 outmf <- paste0(args[1], "_femcase.map")
 # 
 awkCall <- paste0("awk '{$6=$5; print $0}' ", inpedf, " > ", outpf)
 rt <- system(awkCall, intern=F)
-
+if (rt!=0) {
+    stop("awk operation error")
+}
 file.copy(inmapf, outmf)
+
+# But if it was bed, if's not over, we need to convert back
+if(!foundped) {
+    print("So, no ped file was found with the that prefix, try the bed/bim/fam trio")
+    outfpfx <- paste0(args[1], "_femcase")
+    plinkCmd <- paste0(plinkbin, humopts, " --file ", outfpfx, " --recode --out ", outfpfx)
+    rt <- system(plinkCmd, intern=FALSE)
+    if (rt!=0) {
+        stop("plink ped to bed error.")
+    }
+}
+# that's it
